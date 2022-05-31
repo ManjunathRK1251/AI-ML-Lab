cac578229c847140eff21536a122c1adfe21a265
Manjunath R K <rkmanjunath10000@gmail.com>
2022-05-31 18:52:52 +0530

    Add comments to Best first search

--- BestFirstSearch.py
+++ BestFirstSearch.py
@@ -1,19 +1,34 @@
 from queue import PriorityQueue
 v = 14
-graph = [[] for i in range(v)]
 
+#create a list of lists, each of the list represents ith node's children and it's corresponding cost to the children from that node
+graph = [[] for i in range(v)]
+# print(graph)
 
 def best_first_search(source, target, n):
+    #initially mark all the vertices as not visited by initializing the entire list with False 
 	visited = [False] * n
+
+    #Now we have visited the first vertex , so mark it as visited
 	visited[0] = True
+
+    #creation of priority queue
 	pq = PriorityQueue()
+
+    #Add the source to priority queue
 	pq.put((0, source))
+
+    #Iterate the priority queue until it is empty
 	while pq.empty() == False:
-		u = pq.get()[1]
+		# w = pq.get() --> it returns a tuple
+		# print(w)
+		# u = w[1]
+		u = pq.get()[1] #we are accessing the 2nd element of the tuple
 		print(u, end=" ")
 		if u == target:
 			break
 
+		#We will iterate through all the children of the node u, and add each of the child to the priorityQueue and mark them as visited
 		for v, c in graph[u]:
 			if visited[v] == False:
 				visited[v] = True
@@ -22,9 +37,13 @@ def best_first_search(source, target, n):
 
 
 def addedge(x, y, cost):
+	#Adding the cost of reaching child y from node x 
 	graph[x].append((y, cost))
+
+	#Adding the cost of reaching child x from node y
 	graph[y].append((x, cost))
 
+#Creating the graph
 addedge(0, 1, 3)
 addedge(0, 2, 6)
 addedge(0, 3, 5)
@@ -39,6 +58,7 @@ addedge(9, 11, 1)
 addedge(9, 12, 10)
 addedge(9, 13, 2)
 
+# print(graph)
 source = 0
 target = 9
 best_first_search(source, target, v)
